e391b034e381a4671b636e9766f41ea3e3711e0d
lucasoliveirabs <lucasoliveirabs95@gmail.com>
2024-10-29 22:35:03 -0300

    feat: add documentation at '/swagger

--- lacrei-backend/backend/lacrei-core/urls.py
+++ lacrei-backend/backend/lacrei-core/urls.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Lacrei Saúde API",
+        default_version='v1',
+        description="Documentação das APIs de Consulta e Profissional",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contato@lacrei.com"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('consulta.urls')),
-    path('api/', include('profissional.urls')),
+    path('api/consulta/', include('consulta.urls')),
+    path('api/profissional/', include('profissional.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
